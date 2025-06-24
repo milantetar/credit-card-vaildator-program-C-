f15831b41bbc08b60ae3cfcabc4fb998e9254dcd
milan tetar <milantetar@gmail.com>
2025-06-24 13:55:44 +0530

    Add project files.

---
+++ credit card vaildator program C++/credit card vaildator program C++.cpp
@@ -0,0 +1,32 @@
+// credit card vaildator program C++.cpp 
+#include <iostream>
+
+int getDigit(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string catdNumber);
+
+int main()
+{   
+    std::string cardNumber;
+    int result = 0;
+    std::cout <<"Enter Your Credit card Number : "<<std::endl;
+    std::cin >> cardNumber;
+
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber) ;
+    return 0;
+}
+
+int getDigit(const int number)
+{
+    return 0;
+}
+
+int sumOddDigits(const std::string cardNumber)
+{
+    return 0;
+}
+
+int sumEvenDigits(const std::string catdNumber)
+{
+    return 0;
+}
