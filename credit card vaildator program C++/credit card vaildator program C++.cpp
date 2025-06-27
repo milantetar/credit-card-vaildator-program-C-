0dbcdf7edd6166c3f40ed54b2a613e116e05e391
milan tetar <milantetar@gmail.com>
2025-06-27 13:02:38 +0530

    Add credit card validation and fix digit summation logic
    - Implement validation logic in the `main` function to check if the credit card number is valid based on the Luhn algorithm.
    - Update `getDigit` to return the sum of the last two digits of the input number.
    - Implement `sumOddDigits` to calculate the sum of digits at odd positions.
    - Correct parameter name in `sumEvenDigits` and update logic to sum digits at even positions correctly.

--- credit card vaildator program C++/credit card vaildator program C++.cpp
+++ credit card vaildator program C++/credit card vaildator program C++.cpp
@@ -13,27 +13,44 @@ int main()
     std::cin >> cardNumber;
 
     result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber) ;
+
+    if (result % 10 == 0)
+    {
+        std::cout << cardNumber << " is Valid";
+    }
+    else
+    {
+        std::cout << cardNumber << " is  not Valid";
+    }
     return 0;
 }
 
 int getDigit(const int number)
 {
-    return 0;
+    return number % 10 + (number / 10 % 10);
 }
 
 int sumOddDigits(const std::string cardNumber)
 {
-    return 0;
+
+    int sum = 0;
+
+    for (int i = cardNumber.size() - 1; i >= 0; i -= 2)
+    {
+        sum += (cardNumber[i] - '0');
+
+    }
+    return sum;
 }
 
-int sumEvenDigits(const std::string catdNumber)
+int sumEvenDigits(const std::string cardNumber)
 {
     int sum = 0;
 
-    for (int i = catdNumber.size() - 2; i >= 0; i -= 2) 
+    for (int i = cardNumber.size() - 2; i >= 0; i -= 2)
     {
-        sum += getDigit();
+        sum += getDigit((cardNumber[i] - '0') * 2);
 
     }
-    return 0;
+     return sum;
 }
